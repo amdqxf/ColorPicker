16f293751a4dda241573f7c056d096ce7e3dd0fa
Allison Dolan <allisondolan@Allisons-MacBook-Pro.local>
2018-10-02 21:28:21 -0500

    First Commit

---
+++ ColorPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorPicker
+//
+//  Created by Allison Dolan on 10/2/18.
+//  Copyright © 2018 Allison Dolan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
